867db51d13071e8ef8a66fdff7a28e00cea4b88e
rahul2192 <rahul_verma2192@yahoo.com>
2021-03-24 02:24:11 -0400

    this is my tims war file

---
+++ target/.generated/org/cruxframework/crux/core/client/ioc/IocContainer_main_largeDisplayArrows.java
@@ -0,0 +1,26 @@
+package org.cruxframework.crux.core.client.ioc;
+
+import org.cruxframework.crux.core.client.screen.views.View;
+import com.google.gwt.core.client.GWT;
+
+public class IocContainer_main_largeDisplayArrows extends org.cruxframework.crux.core.client.ioc.IocContainer {
+  public IocContainer_main_largeDisplayArrows(View view){
+    super(view);
+  }
+  public  timhortons.client.MainController.MainView gettimhortons_client_MainController_MainView(org.cruxframework.crux.core.client.ioc.IoCResource.Scope scope, String subscope){
+    timhortons.client.MainController.MainView result = _getScope(scope).getValue(new org.cruxframework.crux.core.client.ioc.IocProvider<timhortons.client.MainController.MainView>(){
+      public timhortons.client.MainController.MainView get(){
+        return GWT.create(timhortons.client.MainController.MainView.class);
+      }
+    }, "timhortons.client.MainController.MainView", subscope, 
+    new IocScope.CreateCallback<timhortons.client.MainController.MainView>(){
+      public void onCreate(timhortons.client.MainController.MainView newObject){
+      }
+    }
+    );
+    if (scope != org.cruxframework.crux.core.client.ioc.IoCResource.Scope.SINGLETON && result.getBoundCruxViewId() == null){
+      result.bindCruxView(this.getBoundCruxViewId());
+    }
+    return result;
+  }
+}
